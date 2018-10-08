e0088b96a8fbce6a53755293224b11b1df9f44f4
troy351 <914053923@qq.com>
2018-10-08 15:23:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h3>
+      This tool was designed to optimize json file exported by
+      <a href="https://threejs.org/editor"
+         target="_blank">threejs editor</a>
+      to reduce its size and improve performance.
+    </h3>
+    <p>step 1: please choose <code>app.json</code> file below
+      <input type="file"
+             id="fileInput"
+             accept="application/json"></p>
+  </div>
+</template>
+
+<script src="./App.ts">
+</script>
