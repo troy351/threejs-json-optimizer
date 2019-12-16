6d47c765666dde570d1ff806becfe6c4ebbd4af0
troy351 <914053923@qq.com>
2019-12-16 14:19:35 +0800

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
   root: true,
-
   env: {
-    node: true,
+    node: true
   },
-
-  extends: ['plugin:vue/essential', '@vue/standard'],
-
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
   },
-
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 }
