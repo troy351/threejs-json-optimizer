99615da85e7652fb2b2c7772e9070a03086ba02c
troy351 <914053923@qq.com>
2020-05-18 13:34:54 +0800

    optimize image uri display in search result

--- src/helper/usage.js
+++ src/helper/usage.js
@@ -22,20 +22,25 @@ export function findUUIDUsage(json, str) {
 
   return logs.length
     ? logs
-        .map(j =>
-          JSON.stringify(
-            j,
-            (key, val) => {
-              // make long number string shorter
-              if (Array.isArray(val) && typeof val[0] === 'number')
-                return val.length > 5
-                  ? JSON.stringify(val.slice(0, 5)).slice(0, -1) + ' ...]'
-                  : JSON.stringify(val)
-              return val
-            },
-            2,
-          ),
-        )
-        .join(`\n\n${'-'.repeat(50)}\n\n`)
+      .map(j =>
+        JSON.stringify(
+          j,
+          (key, val) => {
+            // make long number string shorter
+            if (Array.isArray(val) && typeof val[0] === 'number')
+              return val.length > 5
+                ? JSON.stringify(val.slice(0, 5)).slice(0, -1) + ' ...]'
+                : JSON.stringify(val)
+
+            // make image data uri shorter
+            if (typeof val === 'string' && val.startsWith('data:image'))
+              return val.slice(0, 100) + ' ...'
+
+            return val
+          },
+          2,
+        ),
+      )
+      .join(`\n\n${'-'.repeat(50)}\n\n`)
     : 'Nothing found'
 }
